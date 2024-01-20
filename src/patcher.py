90c98eadf7016b65e084648ff804f148bed813b0
Eric Coleman <eric@epiccoleman.com>
2024-01-20 00:49:38 -0500

    Refactor Patcher class to include a multipatch_path parameter in the constructor.

--- src/patcher.py
+++ src/patcher.py
@@ -1,9 +1,10 @@
 import os
 
 class Patcher:
-    def __init__(self, clean_rom_path, hack_path):
+    def __init__(self, clean_rom_path, hack_path, multipatch_path):
         self.clean_rom_path = clean_rom_path
         self.hack_path = hack_path
+        self.multipatch_path = multipatch_path
 
     def find_bps_file(self):
         for root, dirs, files in os.walk(self.hack_path):
