3ceef74f259954eeb7d8a5a63a17783bdc384291
Eric Coleman <eric@epiccoleman.com>
2024-01-20 00:49:41 -0500

    aider: Add `patch_rom` method to `Patcher` class to find the .bps file, calculate the output name, and invoke the binary at `multipatch_path` with specified arguments.
    # Aider chat conversation:
    
    ASSISTANT: To add the `patch_rom` method to the `Patcher` class, we need to modify the `src/patcher.py` file. Specifically, we need to add a method that uses `find_bps_file` to find the patch file, calculates the output name based on the last portion of `hack_path`, and then invokes the binary at `multipatch_path` with the specified arguments.
    
    Here is the *SEARCH/REPLACE block* for the change:
    
    <source>python
    src/patcher.py
    <<<<<<< SEARCH
    import os
    
    class Patcher:
        def __init__(self, clean_rom_path, hack_path, multipatch_path):
            self.clean_rom_path = clean_rom_path
            self.hack_path = hack_path
            self.multipatch_path = multipatch_path
    
        def find_bps_file(self):
            for root, dirs, files in os.walk(self.hack_path):
                for file in files:
                    if file.endswith(".bps"):
                        return os.path.join(root, file)
            return None
    =======
    import os
    import subprocess
    
    class Patcher:
        def __init__(self, clean_rom_path, hack_path, multipatch_path):
            self.clean_rom_path = clean_rom_path
            self.hack_path = hack_path
            self.multipatch_path = multipatch_path
    
        def find_bps_file(self):
            for root, dirs, files in os.walk(self.hack_path):
                for file in files:
                    if file.endswith(".bps"):
                        return os.path.join(root, file)
            return None
    
        def patch_rom(self):
            patch_file = self.find_bps_file()
            if patch_file is None:
                print("No .bps file found in hack_path.")
                return
    
            output_name = os.path.basename(self.hack_path)
            subprocess.run([self.multipatch_path, "--apply", patch_file, self.clean_rom_path, f"{output_name}.smc"])
    >>>>>>> REPLACE
    </source>
    
    This change will add a method `patch_rom` to the `Patcher` class. This method will find the .bps file, calculate the output name, and then invoke the binary at `multipatch_path` with the specified arguments. If no .bps file is found, it will print a message and return.

--- src/patcher.py
+++ src/patcher.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 class Patcher:
     def __init__(self, clean_rom_path, hack_path, multipatch_path):
@@ -12,3 +13,12 @@ class Patcher:
                 if file.endswith(".bps"):
                     return os.path.join(root, file)
         return None
+
+    def patch_rom(self):
+        patch_file = self.find_bps_file()
+        if patch_file is None:
+            print("No .bps file found in hack_path.")
+            return
+
+        output_name = os.path.basename(self.hack_path)
+        subprocess.run([self.multipatch_path, "--apply", patch_file, self.clean_rom_path, f"{output_name}.smc"])
